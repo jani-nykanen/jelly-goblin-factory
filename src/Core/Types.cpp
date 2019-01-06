0c1c07052940d6f5a53d3b3eaec6348358310d9e
jani-nykanen <jnyknn@gmail.com>
2019-01-06 22:40:56 +0200

    Fixed point types, not tested yet

---
+++ src/Core/Types.cpp
@@ -0,0 +1,86 @@
+// Generic types
+// (c) 2019 Jani Nykänen
+
+#include "Types.hpp"
+
+#include <cmath>
+
+
+// Constructor
+Mat3Fixed::Mat3Fixed() {
+
+    identity();
+}
+
+
+// Constructor
+void Mat3Fixed::identity() {
+
+    m11 = FIXED_PRECISION; m21 = 0; m31 = 0;
+	m12 = 0; m22 = FIXED_PRECISION; m32 = 0;
+	m13 = 0; m23 = 0; m33 = FIXED_PRECISION;
+}
+
+
+// Multiply
+Vec2Fixed Mat3Fixed::mul(Vec2Fixed p) {
+
+    Vec2Fixed ret;
+
+    ret.x = m11 * p.x + m21 * p.y + m31 * FIXED_PRECISION;
+    ret.y = m12 * p.x + m22 * p.y + m32 * FIXED_PRECISION;
+
+    return ret;
+}
+Mat3Fixed Mat3Fixed::mul(Mat3Fixed M) {
+
+    Mat3Fixed A;
+
+    A.m11 = m11 * M.m11 + m21 * M.m12 + m31 * M.m13;
+	A.m21 = m11 * M.m21 + m21 * M.m22 + m31 * M.m23;
+	A.m31 = m11 * M.m31 + m21 * M.m32 + m31 * M.m33;
+
+	A.m12 = m12 * M.m11 + m22 * M.m12 + m32 * M.m13;
+	A.m22 = m12 * M.m21 + m22 * M.m22 + m32 * M.m23;
+	A.m32 = m12 * M.m31 + m22 * M.m32 + m32 * M.m33;
+
+	A.m13 = m13 * M.m11 + m23 * M.m12 + m33 * M.m13;
+	A.m23 = m13 * M.m21 + m23 * M.m22 + m33 * M.m23;
+	A.m33 = m13 * M.m31 + m23 * M.m32 + m33 * M.m33;
+
+    A.m11 /= FIXED_PRECISION; A.m12 /= FIXED_PRECISION; A.m13 /= FIXED_PRECISION;
+    A.m21 /= FIXED_PRECISION; A.m22 /= FIXED_PRECISION; A.m23 /= FIXED_PRECISION;
+    A.m31 /= FIXED_PRECISION; A.m32 /= FIXED_PRECISION; A.m33 /= FIXED_PRECISION;
+
+    return A;
+}
+
+
+// Set to rotation matrix
+void Mat3Fixed::rotate(int angle) {
+
+    FixedPoint c = floatToFixed(cosf(angle / 180.0f * M_PI));
+	FixedPoint s = floatToFixed(sinf(angle / 180.0f * M_PI));
+
+	m11 = c; m21 =-s; m31 = 0;
+	m12 = s; m22 = c; m32 = 0;
+	m13 = 0; m23 = 0; m33 = FIXED_PRECISION;
+}
+
+
+// Scale
+void Mat3Fixed::scale(FixedPoint x, FixedPoint y) {
+
+    m11 = x; m21 = 0; m31 = 0;
+	m12 = 0; m22 = y; m32 = 0;
+	m13 = 0; m23 = 0; m33 = FIXED_PRECISION;
+}
+
+
+// Translate
+void Mat3Fixed::translate(FixedPoint x, FixedPoint y) {
+
+    m11 = FIXED_PRECISION; m21 = 0; m31 = x;
+	m12 = 0; m22 = FIXED_PRECISION; m32 = y;
+	m13 = 0; m23 = 0; m33 = FIXED_PRECISION;
+}
--- src/Core/Types.hpp
+++ src/Core/Types.hpp
@@ -4,6 +4,9 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
+#define FIXED_PRECISION 256
+
+// Primitive types
 typedef signed char int8;
 typedef unsigned char uint8;
 typedef signed short int16;
@@ -12,6 +15,65 @@ typedef signed int int32;
 typedef unsigned int uint32;
 typedef signed long int64;
 typedef unsigned long uint64;
+
+// "Fixed point" number
+typedef int FixedPoint;
+
+// To int
+inline int fixedToInt(FixedPoint fp) {
+    return fp / FIXED_PRECISION;
+}
+// Float to fixed
+inline FixedPoint floatToFixed(float f) {
+
+    return (FixedPoint)(f * FIXED_PRECISION);
+}
+
+
+// Fixed point vector
+struct Vec2Fixed {
+
+    // Components
+    int x, y;
+
+    // Constructor
+    inline Vec2Fixed(){x=0;y=0;}
+    inline Vec2Fixed(int x, int y) {
+        this->x=x*FIXED_PRECISION; 
+        this->y=y*FIXED_PRECISION;
+    };
+
+    // Get components
+    inline int getXInt(){return x / FIXED_PRECISION;}
+    inline int getYInt(){return y / FIXED_PRECISION;}
+};
+
+
+// Fixed point 3x3 matrix
+struct Mat3Fixed {
+
+    // Components
+    int m11, m21, m31,
+        m12, m22, m32,
+        m13, m23, m33;
+
+    // Constructor
+    Mat3Fixed();
+
+    // Set to the identity matrix
+    void identity();
+
+    // Multiply
+    Vec2Fixed mul(Vec2Fixed p);
+    Mat3Fixed mul(Mat3Fixed M);
+
+    // Set to rotation matrix
+    void rotate(int angle);
+    // Scale
+    void scale(FixedPoint x, FixedPoint y);
+    // Translate
+    void translate(FixedPoint x, FixedPoint y);
+};
 
 #endif // __TYPES_H__
  
